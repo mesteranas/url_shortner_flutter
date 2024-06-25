23227ec1d829ad25eb6b0d61362da21a5af59bb8
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-25 18:41:07 +0300

    25/06/2024

---
+++ url_shortner/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="url shortner";
+  static const appName="url_shortner_flutter";
+  static const appDirName="url_shortner";
+  static const version=1.0;
+  static const description="a app makes you to short urls ";
+}
---
+++ url_shortner/lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+import 'settings.dart';
+import 'package:flutter/widgets.dart';
+
+import 'language.dart';
+import 'package:http/http.dart' as http;
+import 'viewText.dart';
+import 'app.dart';
+import 'contectUs.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() async{
+  await WidgetsFlutterBinding.ensureInitialized();
+  await Language.runTranslation();
+  runApp(test());
+}
+class test extends StatefulWidget{
+  const test({Key?key}):super(key:key);
+  @override
+  State<test> createState()=>_test();
+}
+class _test extends State<test>{
+  String result="";
+  TextEditingController _URL=TextEditingController();
+  var _=Language.translate;
+  _test();
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      locale: Locale(Language.languageCode),
+      title: App.name,
+      themeMode: ThemeMode.system,
+      home:Builder(builder:(context) 
+    =>Scaffold(
+      appBar:AppBar(
+        title: const Text(App.name),), 
+        drawer: Drawer(
+          child:ListView(children: [
+          DrawerHeader(child: Text(_("navigation menu"))),
+          ListTile(title:Text(_("settings")) ,onTap:() async{
+            await Navigator.push(context, MaterialPageRoute(builder: (context) =>SettingsDialog(this._) ));
+            setState(() {
+              
+            });
+          } ,),
+          ListTile(title: Text(_("contect us")),onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>ContectUsDialog(this._)));
+          },),
+          ListTile(title: Text(_("donate")),onTap: (){
+            launch("https://www.paypal.me/AMohammed231");
+          },),
+  ListTile(title: Text(_("visite project on github")),onTap: (){
+    launch("https://github.com/mesteranas/"+App.appName);
+  },),
+  ListTile(title: Text(_("license")),onTap: ()async{
+    String result;
+    try{
+    http.Response r=await http.get(Uri.parse("https://raw.githubusercontent.com/mesteranas/" + App.appName + "/main/LICENSE"));
+    if ((r.statusCode==200)) {
+      result=r.body;
+    }else{
+      result=_("error");
+    }
+    }catch(error){
+      result=_("error");
+    }
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewText(_("license"), result)));
+  },),
+  ListTile(title: Text(_("about")),onTap: (){
+    showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(title: Text(_("about")+" "+App.name),content:Center(child:Column(children: [
+        ListTile(title: Text(_("version: ") + App.version.toString())),
+        ListTile(title:Text(_("developer:")+" mesteranas")),
+        ListTile(title:Text(_("description:") + App.description))
+      ],) ,));
+    });
+  },)
+        ],) ,),
+        body:Container(alignment: Alignment.center
+        ,child: Column(children: [
+          TextFormField(controller: _URL,decoration: InputDecoration(labelText: _("URL")),),
+          ElevatedButton(onPressed: () async{
+              final String apiUrl = "http://tinyurl.com/api-create.php";
+
+  try {
+    final response = await http.get(
+      Uri.parse('$apiUrl?url=${Uri.encodeComponent(_URL.text)}')
+    );
+
+    if (response.statusCode == 200) {
+      result=response.body;
+    } else {
+      result="can't short this link";
+    }
+  } catch (e) {
+    result="can't short this link";
+  }
+  setState(() {
+    
+  });
+          }, child: Text(_("short"))),
+          Text(result)
+    ])),)));
+  }
+}
